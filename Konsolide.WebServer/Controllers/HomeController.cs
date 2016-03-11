2e8668cd80225c2a600977b3ce61a5ca411d7893
zaferakkose <zaferakkose@hotmail.com>
2016-03-11 14:24:42 +0200

    sol menü getirildi

---
+++ Konsolide.WebServer/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Konsolide.WebServer.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Konsolide.WebServer.Controllers
+{
+    
+        public class HomeController : KonsolideControllerBase
+        {
+            #region Constructor
+
+            public HomeController()
+            {
+
+            }
+
+            #endregion
+
+            #region Private Members
+
+            #endregion
+
+            #region Public Members
+
+            #endregion
+
+            #region Methods
+
+            public ActionResult Spa()
+            {
+
+                try
+                {
+                    ViewBag.Title = this.WebApplicationManager.Framework.Context.Product.CustomerProductName;
+                    ViewBag.SessionStart = this.WebApplicationManager.Framework.Context.CurrentUser.SessionStart;
+                }
+                catch (Exception)
+                {
+                    RedirectToAction("Login", "Account");
+                }
+
+
+                return View();
+            }
+
+           
+            #endregion
+
+        }
+  
+}
---
+++ Konsolide.WebServer/Controllers/UsersController.cs
@@ -0,0 +1,114 @@
+﻿using Konsolide.WebServer.Base;
+using Surat.Base.Model.Entities;
+using Surat.Base.Repositories;
+using Surat.Common.Data;
+using Konsolide.WebServer.Application;
+using Konsolide.WebServer.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+namespace Surat.WebServer.Controllers
+{
+    public class UsersController :KonsolideControllerBase
+    {
+        #region Constructor
+
+        public UsersController()
+        {
+
+        }
+
+        #endregion
+
+        #region Private Members
+
+        #endregion
+
+        #region Public Members
+
+        #endregion
+
+        #region Methods
+       
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        public ActionResult GetUsers(int pageSize,int skip)
+        {
+            
+            try
+            {
+                var users = this.WebApplicationManager.Framework.Security.User.GetUsersActive();
+               var total = users.Count();
+               var data =users.OrderBy(m=>m.Id).Skip(skip).Take(pageSize).ToList();
+               return Json(new { total = total, data = data },JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception exception)
+            {            
+                Response.StatusCode = 500;
+                return Json(new { Result = this.WebApplicationManager.GetGlobalizationKeyValue(this.WebApplicationManager.Framework.Context.SystemId,Constants.Message.OperationNotCompleted) + " " + this.PublishException(exception) });
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Add(SuratUser user)
+        {
+            try
+            {
+             
+                    this.WebApplicationManager.Framework.Security.SaveUser(user);
+
+                    return Json(new { Result = "Kullanıcı başarılı bir şekilde oluşturuldu." }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception exception)
+            {           
+                Response.StatusCode = 500;
+                return Json(new { Result = this.WebApplicationManager.GetGlobalizationKeyValue(this.WebApplicationManager.Framework.Context.SystemId,Constants.Message.OperationNotCompleted) + " " + this.PublishException(exception) });
+            }
+        }
+
+        [HttpPost]
+        public JsonResult Update(SuratUser user)
+        {
+            try
+            {
+
+                this.WebApplicationManager.Framework.Security.SaveUser(user);
+
+                return Json(new { Result = "Kullanıcı bilgileri başarılı bir şekilde güncellendi." }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception exception)
+            {
+                Response.StatusCode = 500;
+                return Json(new { Result = this.WebApplicationManager.GetGlobalizationKeyValue(this.WebApplicationManager.Framework.Context.SystemId,Constants.Message.OperationNotCompleted) + " " + this.PublishException(exception) });
+            }
+        }
+
+        [HttpPost]
+        public JsonResult Delete(SuratUser users)
+        {
+            try
+            {
+               
+                this.WebApplicationManager.Framework.Security.DeleteUser(users);
+
+                return Json(new {Result="Kayıt başarılı bir şekilde silindi"},JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception exception)
+            {            
+                Response.StatusCode = 500;
+                return Json(new { Result = this.WebApplicationManager.GetGlobalizationKeyValue(this.WebApplicationManager.Framework.Context.SystemId,Constants.Message.OperationNotCompleted) + " " + this.PublishException(exception) });
+            }
+        }
+
+        #endregion
+    }
+}
