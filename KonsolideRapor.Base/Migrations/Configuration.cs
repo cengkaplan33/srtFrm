a19a74b4b705b695be75c2ca0ae704892c8957ee
zaferakkose <zaferakkose@hotmail.com>
2016-03-24 10:19:46 +0200

    impr-bank-seed-method-migration

--- KonsolideRapor.Base/Migrations/Configuration.cs
+++ KonsolideRapor.Base/Migrations/Configuration.cs
@@ -1,5 +1,7 @@
 namespace KonsolideRapor.Base.Migrations
 {
+    using Surat.Base.Model.Entities;
+    using Surat.Common.Utilities;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -9,23 +11,127 @@ namespace KonsolideRapor.Base.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(KonsolideRapor.Base.Model.KonsolideRaporDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            
+           
+            this.AddBanks(context);
+        }
+        private void AddBanks(KonsolideRapor.Base.Model.KonsolideRaporDbContext context)
+        {
+            context.Bankalar.AddOrUpdate(p => p.Id, new Bank
+            {
+                Id=1,
+                Code = "1",
+                Name = "Akbank",
+                IsActive = true,
+                InsertedByUser = 1,
+                InsertedDate = TimeUtility.GetCurrentDateTime(),
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            });
+            context.Bankalar.AddOrUpdate(p => p.Id, new Bank
+            {
+                Id = 2,
+                Code = "2",
+                Name = "Albaraka",
+                IsActive = true,
+                InsertedByUser = 1,
+                InsertedDate = TimeUtility.GetCurrentDateTime(),
+
+            });
+            context.Bankalar.AddOrUpdate(p => p.Id, new Bank
+            {
+                Id = 3,
+                Code = "3",
+                Name = "Bankasya",
+                IsActive = true,
+                InsertedByUser = 1,
+                InsertedDate = TimeUtility.GetCurrentDateTime(),
+
+            });
+            context.Bankalar.AddOrUpdate(p => p.Id, new Bank
+            {
+                Id = 4,
+                Code = "4",
+                Name = "Finansbank",
+                IsActive = true,
+                InsertedByUser = 1,
+                InsertedDate = TimeUtility.GetCurrentDateTime(),
+
+            });
+            context.Bankalar.AddOrUpdate(p => p.Id, new Bank
+            {
+                Id = 5,
+                Code = "5",
+                Name = "Garanti",
+                IsActive = true,
+                InsertedByUser = 1,
+                InsertedDate = TimeUtility.GetCurrentDateTime(),
+
+            });
+            context.Bankalar.AddOrUpdate(p => p.Id, new Bank
+            {
+                Id = 6,
+                Code = "6",
+                Name = "Ýþ Bankasý",
+                IsActive = true,
+                InsertedByUser = 1,
+                InsertedDate = TimeUtility.GetCurrentDateTime(),
+
+            });
+            context.Bankalar.AddOrUpdate(p => p.Id, new Bank
+            {
+                Id = 7,
+                Code = "7",
+                Name = "Kuveyttürk",
+                IsActive = true,
+                InsertedByUser = 1,
+                InsertedDate = TimeUtility.GetCurrentDateTime(),
+
+            });
+            context.Bankalar.AddOrUpdate(p => p.Id, new Bank
+            {
+                Id = 8,
+                Code = "8",
+                Name = "Türkiye Finans",
+                IsActive = true,
+                InsertedByUser = 1,
+                InsertedDate = TimeUtility.GetCurrentDateTime(),
+
+            });
+            context.Bankalar.AddOrUpdate(p => p.Id, new Bank
+            {
+                Id = 9,
+                Code = "9",
+                Name = "Vakýfbank",
+                IsActive = true,
+                InsertedByUser = 1,
+                InsertedDate = TimeUtility.GetCurrentDateTime(),
+
+            });
+            context.Bankalar.AddOrUpdate(p => p.Id, new Bank
+            {
+                Id = 10,
+                Code = "10",
+                Name = "Yapýkredi",
+                IsActive = true,
+                InsertedByUser = 1,
+                InsertedDate = TimeUtility.GetCurrentDateTime(),
+
+            });
+            context.Bankalar.AddOrUpdate(p => p.Id, new Bank
+            {
+                Id = 11,
+                Code = "11",
+                Name = "Ziraat Bankasý",
+                IsActive = true,
+                InsertedByUser = 1,
+                InsertedDate = TimeUtility.GetCurrentDateTime(),
+
+            });
         }
     }
 }
