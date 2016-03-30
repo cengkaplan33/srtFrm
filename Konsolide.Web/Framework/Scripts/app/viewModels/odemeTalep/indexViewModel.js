c16a90e4e714e14a1cda982e449e488a58ae2d4a
zaferakkose <zaferakkose@hotmail.com>
2016-03-30 09:15:00 +0300

    Grid üzerinde ki Düzenle butonunun text özellikleri değiştirildi

--- Konsolide.Web/Framework/Scripts/app/viewModels/odemeTalep/indexViewModel.js
+++ Konsolide.Web/Framework/Scripts/app/viewModels/odemeTalep/indexViewModel.js
@@ -212,7 +212,7 @@ function (kendo, odemeTalepModel, odemeTalepDatasource, odemeTuruDatasource, Ode
                  {
                      command:
                      [
-                     { name: "edit" },
+                     { name: "edit" ,text:{edit:"Düzenle",update:"Kaydet",cancel:"İptal"}},
                      { name: "destroy" }
                      ],
                      title: "İşlemler", width: "170px"
